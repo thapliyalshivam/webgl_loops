e6c7963fe352c91bc10976bd3369e37487abe38b
Shivam Thapliyal <thapliyalshivam@gmail.com>
2019-06-30 00:10:36 +0530

    routing logic

--- module_load.js
+++ module_load.js
@@ -1,14 +1,20 @@
 async function loadModule() {
-  const num = window.location.hash.substr(1) || 1;
-  const module = await
-  import (`./js/loops/main_${num}.js`);
-  document.body.appendChild(module.canvas);
-  document.body.style.margin="0px";
 
+async function getscript(){
+    const num = window.location.hash.substr(1) || 1;
+    const module = await
+    import (`./js/loops/main_${num}.js`);
+    document.body.appendChild(module.canvas);
+    return module;
+}
+
+document.body.style.margin="0px";
+let module = await getscript();
+let head, animationLoop, vbutton;
 
   if (module.heading!=undefined)
   {
-    let head  = document.createElement("h4");
+      head  = document.createElement("h4");
     head.innerHTML = module.heading;
     head.style.position="absolute";
     head.style.position= "absolute";
@@ -19,9 +25,62 @@ async function loadModule() {
     head.style.padding= "8px 55px";
     head.style["border-radius"] = "6px";
     head.style["font-family"]= "arial";
-
     document.body.appendChild(head);
   }
+
+  function update() {
+    // console.log(module);
+    if(module.isVR!=undefined&&module.isVR){
+      vbutton = WEBVR.createButton( module.renderer )
+      document.body.appendChild(  vbutton );
+      module.renderer.setAnimationLoop(module.Render);}
+    else{
+      animationLoop = requestAnimationFrame(update);
+      module.Render();
+        }
+  }
+
+  update();
+
+async function refresh(){
+  if (module && module.canvas)
+ {
+   if(module.isVR!=undefined&&module.isVR){
+     document.body.removeChild(  vbutton );
+   }else{
+     window.cancelAnimationFrame(animationLoop)
+   }
+   document.body.removeChild(module.canvas);
+(module.heading!=undefined&&head!=undefined)&&document.body.removeChild(head);
+
+
+  module = await getscript();
+  if (module.heading!=undefined)
+  {
+      head  = document.createElement("h4");
+    head.innerHTML = module.heading;
+    head.style.position="absolute";
+    head.style.position= "absolute";
+    head.style.top= "0";
+    head.style["font-weight"]="500";
+    head.style.color= "white";
+    head.style.background= "#272bca";
+    head.style.padding= "8px 55px";
+    head.style["border-radius"] = "6px";
+    head.style["font-family"]= "arial";
+    document.body.appendChild(head);
+  }
+  update();
+ }
+
+}
+
+window.addEventListener("click",e=>{
+refresh();
+});
+ window.addEventListener("hashchange",e=>{refresh();});
+}
+
   //
   // if (module.isVR!=undefined&& isVR==true)
   // {
@@ -31,21 +90,8 @@ async function loadModule() {
   //       document.body.appendChild(script);
   // }
 
-    function update() {
-      // console.log(module);
-      if(module.isVR!=undefined&&module.isVR){
-        document.body.appendChild( WEBVR.createButton( module.renderer ) );
-        module.renderer.setAnimationLoop(module.Render);}
-      else{
-        requestAnimationFrame(update);
-        module.Render();
-          }
-    }
-
-    update();
 
 
-}
 //
 // async function init() {
 //   let module = await loadModule();
