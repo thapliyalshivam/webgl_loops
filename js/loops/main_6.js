b1abd97194403813e61d6abb5dc40af0f80e6036
Shivam Thapliyal <thapliyalshivam@gmail.com>
2020-03-31 03:02:46 +0530

    AR TEST

--- js/loops/main_6.js
+++ js/loops/main_6.js
@@ -100,6 +100,22 @@ console.log(material);
 //   gun = object;
 //   init();
 // });
+var controller;
+var geometry = new THREE.CylinderBufferGeometry( 0, 0.05, 0.2, 32 ).rotateX( Math.PI / 2 );
+
+				function onSelect() {
+
+					var material = new THREE.MeshPhongMaterial( { color: 0xffffff * Math.random() } );
+					var mesh = new THREE.Mesh( geometry, material );
+					mesh.position.set( 0, 0, - 0.3 ).applyMatrix4( controller.matrixWorld );
+					mesh.quaternion.setFromRotationMatrix( controller.matrixWorld );
+					scene.add( mesh );
+
+				}
+
+				controller = renderer.xr.getController( 0 );
+				controller.addEventListener( 'select', onSelect );
+				scene.add( controller );
 
 
 let loader = new GLTFLoader();
